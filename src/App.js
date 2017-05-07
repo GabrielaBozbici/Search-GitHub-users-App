04c84558314b0958799a0e9eaa2e0555633f4a92
Gabriela Bozbici <gabrielabozbici@Gheorgheoius-MacBook-Pro.local>
2017-05-07 16:41:05 +0200

    added photo and used Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
 import Form from './components/Form.js';
 import List from './components/List.js';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+injectTapEventPlugin();
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+
 
 //styles
 import './App.less';
@@ -9,9 +17,11 @@ import './App.less';
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Form />
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <Form />
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import List from './List.js';
+import RaisedButton from 'material-ui/RaisedButton';
+import Photo from '../assets/github-octocat';
 
 import axios from 'axios';
 
@@ -9,7 +11,8 @@ export default class Form extends Component{
         this.state = {
             user: '',
             repos: [],
-            statusErr: ''
+            statusErr: '',
+            searchedUser: '',
         }
     }
       handleInputChange(event){
@@ -22,22 +25,27 @@ export default class Form extends Component{
         event.preventDefault();
         var userID = this.state.user;
         this.getUserRepos(userID);
-        console.log('inputul se goleste', this);
-        this.textInput.value = " ";
+        this.textInput.value = "";
+        this.searchedUser = '';
     }
     getUserRepos(userID){
-       axios.get('https://api.github.com/users/' + userID + '/repos').then((response) => {
+        var response = axios.get('https://api.github.com/users/' + userID + '/repos')
+        response.data
+       axios.get('https://api.github.com/users/' + userID + '/repos')
+       .then((response) => {
             console.log('response: ', response);
             this.setState({
                 repos: response.data,
-                statusErr: ''
+                statusErr: '',
+                searchedUser: userID
             }); 
         }).catch((error) => {
             if(error.response.status === 404){
                 console.log('there is an error:')
                 this.setState({
                     statusErr: 404,
-                    repos: []
+                    repos: [],
+                    searchedUser: userID
                 })
             }
             console.log('eroare: ', error.response.status);
@@ -47,29 +55,34 @@ export default class Form extends Component{
     render(){
         return(
             <div className="wrap">
-                <div className="col-md-8 col-sm-8 coll-xs-8 col-offset-1 col-sm-offset-0">
-                    <form className="navbar-form navbar-center" onSubmit={this.handleSubmit.bind(this)}>
+                <h3 className="title text-center">Search GitHub users and see their repositories</h3>
+                <div className="cat"><img src={Photo} className={`text-center ${this.state.repos.length ? 'shrink' : ''}`} /></div>
+                <div className="col-md-12 col-sm-12 coll-xs-8 col-offset-1 col-sm-offset-0">
+                    <form className="navbar-form navbar-center text-center" onSubmit={this.handleSubmit.bind(this)}>
                         <div className="form-group">
                             <input type="text" className="form-control" placeholder="type username" ref={(input) => { this.textInput = input}} onChange={this.handleInputChange.bind(this)} />
                         </div>
-                        <button className="btn btn-default" type="submit">Search user!</button>
+                        <RaisedButton label="Search user" type="submit" primary={true}/>
                     </form>
-                    
-                    <div className="col-md-8 col-sm-8 coll-xs-8 col-offset-1 col-sm-offset-0">
-                        {this.state.statusErr === 404
-                        ? <h4>The GitHub user: {this.state.user} does not exist!</h4> 
+                    <div className="col-md-12 col-sm-12 coll-xs-8 col-xs-offset-1 col-sm-offset-0 text-center content">
+                        {!this.state.user ? <h4>Please write a username!</h4> 
+                        : null}
+                        {this.state.user && this.state.statusErr === 404
+                        ? <h4>The GitHub user: {this.state.searchedUser} does not exist!</h4> 
                         : null }
                         {this.state.repos.length 
-                        ? <div className="text-center">{this.state.repos[0].owner.login} has the following repositories:
-                            {this.state.repos.map(repo => <li key={repo.id}>{repo.name}</li>)}</div>
+                        ? <div className="text-center phrase">
+                            <p className="user"><span>{this.state.repos[0].owner.login}</span> has the following repositories:</p>
+                            {this.state.repos.map(repo => <li key={repo.id}>
+                                <div className="col-md-12 col-sm-12 text-center">{repo.name}</div>
+                                <a target="_blank" href={`${repo.html_url}`} className="text-center">{repo.html_url}</a></li>)}</div>
                         : null}
                     </div>
                 </div>
-                
-
-
+                <div className=" col-md-12 col-sm-12 col-xs-12 bottom-side">
+                    <p className="text-center">&copy; Copyright 2017 Gabriela Bozbici</p>
+                </div>
                 {/*<List repos={this.state.repos} />*/}
-               
             </div>
         )
     }
